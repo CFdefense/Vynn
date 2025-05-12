f55a52d6c0404da7f85955f855d49eb966901a2a
Marko <markopavic7777@gmail.com>
2025-05-11 22:24:40 -0400

    adapter static

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,11 +1,20 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
 
-	kit: {adapter: adapter()}
+	kit: {
+		adapter: adapter({
+			// default options
+			pages: 'build',
+			assets: 'build',
+			fallback: 'index.html',
+			precompress: false,
+			strict: true
+		})
+	}
 };
 
 export default config;
